d889e4b1e1183002ff9edcec7203440bac19f2fc
ahronahron <pasion_ahronjohn@plpasig.edu.ph>
2025-05-01 15:17:27 +0800

    Version 1.12 -Changes: -New modal has been imported -and php connection is good

--- pages/4_Event.php
+++ pages/4_Event.php
@@ -38,143 +38,144 @@
                         </form>
                     </div>
                 </div>
-                    <div class="event-list">
-                         <?php
-                            include '../php/conn.php';
+                <div class="event-list">
+                    <?php
+                        include '../php/conn.php';
 
-                            $result = $conn->query("SELECT * FROM event_table ORDER BY number DESC"); 
+                        $result = $conn->query("SELECT * FROM event_table ORDER BY number DESC"); 
 
-                            while ($row = $result->fetch_assoc()) {
-                                echo "<div class='event-box-details'>";
-                                echo "  <div class='floating-card'>";
-                                echo "      <div class='event-date'>";
-                                echo "          <p class='day'>" .$row['date_start']. "</p>";
-                                echo "          <p class='time'>" .$row['event_start']. "</p>";
-                                echo "      </div>";
-                                echo "      <div class='event-description'>";
-                                echo "          <h3>" .htmlspecialchars($row['event_title']). "</h3>";
-                                echo "          <p>" .htmlspecialchars($row['event_description'])."</p>";
-                                echo "      </div>";
-                                echo "      <div class='date'>";
-                                echo "          <p>" . $row['date_start'] . "</p>"; 
-                                echo "      </div>";
-                                echo "  </div>";
-                                echo "  <div class='even-more-details'>";
-                                echo "      <div class='event-box-row' id='box1'>";
-                                echo "          <div class='event-box-column'>";
-                                echo "              <p> Location: <b> " .htmlspecialchars($row['event_location']). "</b></p>";
-                                echo "              <p> Organization: <b> " .htmlspecialchars($row['organization']). "</b></p>";
-                                echo "              <p> </p>";
-                                echo "              <p> </p>";
-                                echo "          </div>";
-                                echo "          <div class='event-controls' id='box2'>";
-                                echo "             <button class='btn-edit' id='open-registration' onclick='togglePanel()'>Edit</button>";
-                                echo "              <button class='delete'> Delete </button>";
-                                echo "          </div>";
-                                echo "      </div>";
-                                echo "  </div>";
-                                echo "</div>";
-                                }
-                            ?>
-                        <div class="add-button">
-                            <button class="btn-import" id="openModal" onclick="openModal()"> <i class="fa-solid fa-plus"></i> </button>
-                        </div>
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<div class='event-box-details'>";
+                            echo "  <div class='floating-card'>";
+                            echo "      <div class='event-date'>";
+                            echo "          <p class='day'>" .$row['date_start']. "</p>";
+                            echo "          <p class='time'>" .$row['event_start']. "</p>";
+                            echo "      </div>";
+                            echo "      <div class='event-description'>";
+                            echo "          <h3>" .htmlspecialchars($row['event_title']). "</h3>";
+                            echo "          <p>" .htmlspecialchars($row['event_description'])."</p>";
+                            echo "      </div>";
+                            echo "      <div class='date'>";
+                            echo "          <p>" . $row['date_start'] . "</p>"; 
+                            echo "      </div>";
+                            echo "  </div>";
+                            echo "  <div class='even-more-details'>";
+                            echo "      <div class='event-box-row' id='box1'>";
+                            echo "          <div class='event-box-column'>";
+                            echo "              <p> Location: <b> " .htmlspecialchars($row['event_location']). "</b></p>";
+                            echo "              <p> Organization: <b> " .htmlspecialchars($row['organization']). "</b></p>";
+                            echo "              <p> </p>";
+                            echo "              <p> </p>";
+                            echo "          </div>";
+                            echo "          <div class='event-controls' id='box2'>";
+                            echo "             <button class='btn-edit' id='open-registration' onclick='openRegistration()'>Edit</button>";
+                            echo "              <button class='delete'> Delete </button>";
+                            echo "          </div>";
+                            echo "      </div>";
+                            echo "  </div>";
+                            echo "</div>";
+                        }
+                    ?>
+                    <div class="add-button">
+                        <button class="btn-import" id="openModal" onclick="openModal()"> <i class="fa-solid fa-plus"></i> </button>
                     </div>
                 </div>
             </div>
-            <div class="second-page">
-                <!-- Popup for the registration list of attendies -->
-                <div class="registration-table-box" id="importRegistration">
-                    <div class="registration-modal-content">
-                        <div class="registraion-header">
-                        </div>
-                        <form>
-                            <label>First Name:
-                                <input type="text" name="firstName" maxlength="30" required>
-                            </label>
-
-                            <label>Last Name:
-                                <input type="text" name="lastName" maxlength="30" required>
-                            </label>
-
-                            <label>Section:
-                                <input type="text" name="section" maxlength="10">
-                            </label>
-
-                            <label>Year Level:
-                                <input type="text" name="grade" maxlength="10">
-                            </label>
-
-                            <label>Course:
-                                <input type="text" name="course" maxlength="50">
-                            </label>
-
-                            <div class="gender">
-                                <label>Gender:</label>
-                                <input type="radio" name="gender" value="Male" id="male"> 
-                                <label for="male" style="display: inline;">Male</label>
-                                <input type="radio" name="gender" value="Female" id="female"> 
-                                <label for="female" style="display: inline;">Female</label>
-                            </div>
-
-                            <label>Email:
-                                <input type="email" name="email" required>
-                            </label>
-
-                            <label>Phone Number:
-                                <input type="tel" name="phone" maxlength="10" pattern="[0-9]{10}" required>
-                            </label>
-
-                            <label>Address:
-                                <textarea name="address" maxlength="100" rows="3"></textarea>
-                            </label>
-
-                            <h3>Qualifications</h3>
-                            <table id="qualificationsTable">
-                                <thead>
-                                    <tr>
-                                        <th>Sl. No</th>
-                                        <th>Examination</th>
-                                        <th>Board</th>
-                                        <th>Percentage</th>
-                                        <th>Year of Passing</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td><input type="text" name="exam1" maxlength="15"></td>
-                                        <td><input type="text" name="board1" maxlength="15"></td>
-                                        <td><input type="text" name="percentage1" maxlength="5"></td>
-                                        <td><input type="text" name="year1" maxlength="4"></td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td><input type="text" name="exam2" maxlength="15"></td>
-                                        <td><input type="text" name="board2" maxlength="15"></td>
-                                        <td><input type="text" name="percentage2" maxlength="5"></td>
-                                        <td><input type="text" name="year2" maxlength="4"></td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td><input type="text" name="exam3" maxlength="15"></td>
-                                        <td><input type="text" name="board3" maxlength="15"></td>
-                                        <td><input type="text" name="percentage3" maxlength="5"></td>
-                                        <td><input type="text" name="year3" maxlength="4"></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-
-                            <div class="table-actions">
-                                <button type="button" class="add-row-btn" id="addQualificationBtn">+ Add Qualification</button>
-                            </div>
-
-                            <button type="submit" class="submit-btn">Submit Registration</button>
-                        </form>
+        </div>
+        <div class="second-page">
+            <!-- Popup for the registration list of attendies -->
+            <div class="registration-table-box" id="importRegistration">
+                <div class="registration-modal-content">
+                    <div class="registraion-header">
                     </div>
+                    <form>
+                        <label>First Name:
+                            <input type="text" name="firstName" maxlength="30" required>
+                        </label>
+
+                        <label>Last Name:
+                            <input type="text" name="lastName" maxlength="30" required>
+                        </label>
+
+                        <label>Section:
+                            <input type="text" name="section" maxlength="10">
+                        </label>
+
+                        <label>Year Level:
+                            <input type="text" name="grade" maxlength="10">
+                        </label>
+
+                        <label>Course:
+                            <input type="text" name="course" maxlength="50">
+                        </label>
+
+                        <div class="gender">
+                            <label>Gender:</label>
+                            <input type="radio" name="gender" value="Male" id="male"> 
+                            <label for="male" style="display: inline;">Male</label>
+                            <input type="radio" name="gender" value="Female" id="female"> 
+                            <label for="female" style="display: inline;">Female</label>
+                        </div>
+
+                        <label>Email:
+                            <input type="email" name="email" required>
+                        </label>
+
+                        <label>Phone Number:
+                            <input type="tel" name="phone" maxlength="10" pattern="[0-9]{10}" required>
+                        </label>
+
+                        <label>Address:
+                            <textarea name="address" maxlength="100" rows="3"></textarea>
+                        </label>
+
+                        <h3>Qualifications</h3>
+                        <table id="qualificationsTable">
+                            <thead>
+                                <tr>
+                                    <th>Sl. No</th>
+                                    <th>Examination</th>
+                                    <th>Board</th>
+                                    <th>Percentage</th>
+                                    <th>Year of Passing</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td><input type="text" name="exam1" maxlength="15"></td>
+                                    <td><input type="text" name="board1" maxlength="15"></td>
+                                    <td><input type="text" name="percentage1" maxlength="5"></td>
+                                    <td><input type="text" name="year1" maxlength="4"></td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td><input type="text" name="exam2" maxlength="15"></td>
+                                    <td><input type="text" name="board2" maxlength="15"></td>
+                                    <td><input type="text" name="percentage2" maxlength="5"></td>
+                                    <td><input type="text" name="year2" maxlength="4"></td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td><input type="text" name="exam3" maxlength="15"></td>
+                                    <td><input type="text" name="board3" maxlength="15"></td>
+                                    <td><input type="text" name="percentage3" maxlength="5"></td>
+                                    <td><input type="text" name="year3" maxlength="4"></td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                        <div class="table-actions">
+                            <button type="button" class="add-row-btn" id="addQualificationBtn">+ Add Qualification</button>
+                        </div>
+
+                        <button type="submit" class="submit-btn">Submit Registration</button>
+                    </form>
                 </div>
             </div>
-        </div>                
+        </div>
+    </div>                
+
 
             <!-- This is the popup box for the import of Event System that includes the Event title, Event location, Date, time and Organization-->
             <div id="importModal" class="modal">
@@ -260,15 +261,8 @@
                 </div>
             </div>
         </div>    
-        <!-- Popup for the registration list of attendies -->
-        <div class="registration-table-box">
-            <div class="registration-modal-content">
-                <div class="header">
-                </div>
-                <form>
-                </form>
-            </div>
-        </div>
+
+       
         <script src="../Javascript/popup.js"></script>
         <script src="../Javascript/RandomCodeGenerator.js"></script>
     
